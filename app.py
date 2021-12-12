35642308a0bfe6bbeca6216c660186494e87e2e6
20001676 <79438132+20001676@users.noreply.github.com>
2021-12-11 18:16:50 -0600

    Lab 5 Kimberly Rivera

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+
+import config as cfg
+import pipeline_predict2 as pp
+
+app = Flask(__name__)
+
+@app.route("/saludar")
+def saludar():
+    return jsonify({"username": "kimberly"})
+
+#ruta para predecir.
+@app.route("/predecir", methods=['POST'])
+def predict_from_pp():
+    json_data = request.get_json()
+    dataframe = pd.json_normalize(json_data)
+    data = dataframe
+
+    #cambiar nombre X1stFlrSF y X2ndFlrSF
+    data = data[cfg.FEATURES]
+
+    resultado = pp.predict(data)
+    print(resultado)
+    return jsonify({"Prediccion": resultado})
+
+    
